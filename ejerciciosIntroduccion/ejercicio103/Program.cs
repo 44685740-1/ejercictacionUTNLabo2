5704e2752c26b6eb1f735f7e87b2199e0f81c137
Ahuitz Caracciolo <ahuitzcaracciolo@gmail.com>
2023-03-29 13:20:10 -0300

    agrego ejercio 103 de Introduccion, Los primos(primer commit de pureba)

---
+++ ejerciciosIntroduccion/ejercicio103/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace ejercicio103
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+
+            int numeroIngresado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ListarNumerosPrimos(numeroIngresado));
+            // Parsear el numero y pasarselo metodo
+        }
+
+        public static string ListarNumerosPrimos(int numeroTope)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 2; i <= numeroTope; i++)
+            {
+                // Manejar la logica de si i es primo
+                if (EsPrimo(i))
+                {
+                    // sb.AppendFormat("{0} ,", i);
+                    stringBuilder.Append($"{i} ,");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static bool EsPrimo(int numeroTope)
+        {
+            for (int i = 2; i < numeroTope; i++)
+            {
+                if (numeroTope % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
